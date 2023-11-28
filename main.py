c71b4412081c4860aa74de0d2b798e0557eb9c7f
AndrijanaDejkovic <andrijanadejkovic@elfak.rs>
2023-11-28 19:58:01 +0100

    Promena importa

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from Interface.GameInitializer import intializeGame
-from MinMax.GameState import GameState
+from Controllers.GameState import GameState
 state:GameState = intializeGame(16,16)
 
 printWholeTable(state)
