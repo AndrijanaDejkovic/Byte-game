d48fcff05968f312ed41beb0b60846da28200fd8
AndrijanaDejkovic <andrijanadejkovic@elfak.rs>
2023-11-28 19:39:49 +0100

    Dodat Printer fajl

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from Interface.GameInitializer import intializeGame
 from MinMax.GameState import GameState
-state:GameState = intializeGame(15,15)
+state:GameState = intializeGame(16,16)
+
+printWholeTable(state)
