de3adec88d730ce51d8728507ff192838a45118b
Milica <milica.vidojkovic@elfak.rs>
2023-12-28 16:47:13 +0100

    o

--- Controllers/MinMaxAlg/Heuristics.py
+++ Controllers/MinMaxAlg/Heuristics.py
@@ -1,10 +1,10 @@
 import copy
 from Controllers.GameState import GameState
-from Controllers.TurnController import returnAllValidMovesForSing
+from Controllers.TurnController import returnAllValidMovesForSign
 def evaluateState(state:GameState, whoPlayed=None, whoIsOpponent = None):
     score = 0
     multiplier = -1 if state.currentTurn == "X" else 1
     opponentSign = "O" if state.currentTurn == "X" else "X"
     score+=state.playerScore * 1000 - state.cpuScore * 1000
-    score+= len(returnAllValidMovesForSing(state, state.currentTurn)) - len(returnAllValidMovesForSing(state, opponentSign))
+    score+= len(returnAllValidMovesForSign(state, state.currentTurn)) - len(returnAllValidMovesForSign(state, opponentSign))
     return score * multiplier
